2575dc08bfad507dd6517c8f56f7cbfb2a6fdfce
Jadsia <megankyoung@live.com>
2019-04-18 22:31:54 -0700

    added pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,5 +3,9 @@ class PlacesController < ApplicationController
     def index
         @places = Place.all
     end
+
+    def index
+        @places = Place.order("name").page(params[:page]).per_page(5)
+    end
     
 end
