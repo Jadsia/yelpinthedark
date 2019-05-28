6dbf77506da5533240c897d8c22036b4872a2633
Jadsia <megankyoung@live.com>
2019-05-27 18:35:37 -0700

    added comment model, issues resolved

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -34,17 +34,17 @@ class PlacesController < ApplicationController
   end
 end
 
-  def update
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+def update
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
 
-    @place.update_attributes(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :edit, status: :unprocessable_entity
+  @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
   end
 end
 
